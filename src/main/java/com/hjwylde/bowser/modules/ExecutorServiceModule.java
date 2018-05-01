29cb325d8b7b24e73c6bbcaaff628a5e1bd02820
Henry J. Wylde <hjwylde@gmail.com>
2018-05-01 20:21:59 +1200

    Use a DiscardPolicy with the cached thread pool
    Currently background threads are only used for data loading, not data
    writing. It is therefore safe to just discard rejected execution
    exceptions and do nothing with them.

--- src/main/java/com/hjwylde/bowser/modules/ExecutorServiceModule.java
+++ src/main/java/com/hjwylde/bowser/modules/ExecutorServiceModule.java
@@ -3,7 +3,9 @@ package com.hjwylde.bowser.modules;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Provides an {@link ExecutorService} for use on background threads. If this application were larger it would be better
@@ -11,9 +13,20 @@ import java.util.concurrent.Executors;
  * managed by the {@link com.hjwylde.bowser.ui.frames.bowser.BowserFrame}.
  */
 public final class ExecutorServiceModule {
-    private static final @NotNull ExecutorService INSTANCE = Executors.newCachedThreadPool();
+    private static final @NotNull ExecutorService INSTANCE = createInstance();
 
     public static @NotNull ExecutorService provideExecutorService() {
         return INSTANCE;
+    }
+
+    private static @NotNull ExecutorService createInstance() {
+        // Effectively a cached thread pool (see Executors) with a DiscardPolicy
+        return new ThreadPoolExecutor(
+                0,
+                Integer.MAX_VALUE,
+                60L, TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                new ThreadPoolExecutor.DiscardPolicy()
+        );
     }
 }
