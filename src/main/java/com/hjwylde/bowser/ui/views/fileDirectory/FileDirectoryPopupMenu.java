e8c162a3ad23343558f1edd9e070d355f60b6f1b
Henry J. Wylde <hjwylde@gmail.com>
2018-05-02 15:41:06 +1200

    Add ability to sort directory from right click popup menu

--- src/main/java/com/hjwylde/bowser/ui/views/fileDirectory/FileDirectoryPopupMenu.java
+++ src/main/java/com/hjwylde/bowser/ui/views/fileDirectory/FileDirectoryPopupMenu.java
@@ -10,6 +10,9 @@ final class FileDirectoryPopupMenu extends JPopupMenu {
     private static final @NotNull ResourceBundle RESOURCES = ResourceBundle.getBundle(FileDirectoryPopupMenu.class.getName(), LocaleModule.provideLocale());
     private static final @NotNull String RESOURCE_OPEN = "open";
     private static final @NotNull String RESOURCE_COPY_PATH = "copyPath";
+    private static final @NotNull String RESOURCE_SORT_BY = "sortBy";
+    private static final @NotNull String RESOURCE_SORT_BY_NAME = "sortByName";
+    private static final @NotNull String RESOURCE_SORT_BY_SIZE = "sortBySize";
 
     private final @NotNull FileDirectory.View view;
 
@@ -24,10 +27,21 @@ final class FileDirectoryPopupMenu extends JPopupMenu {
         openMenuItem.addActionListener(e -> new OpenAction(view).run());
         add(openMenuItem);
 
-        addSeparator();
-
         JMenuItem copyPathMenuItem = new JMenuItem(RESOURCES.getString(RESOURCE_COPY_PATH));
         copyPathMenuItem.addActionListener(e -> new CopyPathAction(view).run());
         add(copyPathMenuItem);
+
+        addSeparator();
+
+        JMenu sortByMenu = new JMenu(RESOURCES.getString(RESOURCE_SORT_BY));
+        add(sortByMenu);
+
+        JMenuItem sortByNameMenuItem = new JMenuItem(RESOURCES.getString(RESOURCE_SORT_BY_NAME));
+        sortByNameMenuItem.addActionListener(e -> view.sort(FileNode.NAME_COMPARATOR));
+        sortByMenu.add(sortByNameMenuItem);
+
+        JMenuItem sortBySizeMenuItem = new JMenuItem(RESOURCES.getString(RESOURCE_SORT_BY_SIZE));
+        sortBySizeMenuItem.addActionListener(e -> view.sort(FileNode.SIZE_COMPARATOR));
+        sortByMenu.add(sortBySizeMenuItem);
     }
 }
